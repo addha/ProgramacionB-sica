f1dce5655f8b75e5f3c76bfe4a697f12a95a484b
addha <85791287+addha@users.noreply.github.com>
2022-06-19 13:42:43 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+//REferencia: https://p5js.org/reference/#/p5/windowHeight
+//Variales Globales
+
+//variables tipo Arrglo
+
+//creacion objetos
+var objeto1;
+
+function preload(){
+ 
+}
+
+function setup() {
+  //createCanvas(700,700);
+  createCanvas(windowWidth,windowHeight); //ajusta tamaño  del mapa de la pantalla
+   
+  //Creacion de objetos
+    objeto1 = createSprite(300,140);
+}
+
+
+function draw() {
+  
+  background("pink");
+ 
+ // drawSprites();
+//Propiedadesdelos Objetos
+ /* circle(20,20,20);
+    fill("pink");
+*/
+
+  
+}
